37ba8ae63bdcb9a3f967cc7a9be469674804f391
vinayak8295 <imvinayak1@gmail.com>
2021-03-08 17:18:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageOps
 
 filename = "wanda.png"
 
@@ -6,10 +6,11 @@ img = Image.open(filename)
 img1 = Image.open(filename)
 img2 = Image.open(filename)
 img3 = Image.open(filename)
+img4 = Image.open(filename)
+img5 = Image.open(filename)
+img6 = Image.open(filename)
 
-
-
-# for rotation
+#for rotation
 rotation_angle = input("how much degree you want to rotate")
 rotation_angle = int(rotation_angle)
 
@@ -42,3 +43,40 @@ Width = int(Width)
 
 img2 = img2.crop(box=(startingX,startingY,Width,Height))
 img2.show()
+
+#Horizontal reflection
+img3 = ImageOps.mirror(img3)
+img3.show()
+
+#Vertical reflection
+
+img4 = ImageOps.flip(img4)
+img4.show()
+
+# Read pixels and apply negative transformation
+
+for i in range(0, img5.size[0] - 1):
+    for j in range(0, img5.size[1] - 1):
+        # Get pixel value at (x,y) position of the image
+
+        pixelColorVals = img5.getpixel((i, j));
+
+        # Invert color
+
+        redPixel = 255 - pixelColorVals[0];  # Negate red pixel
+
+        greenPixel = 255 - pixelColorVals[1];  # Negate green pixel
+
+        bluePixel = 255 - pixelColorVals[2];  # Negate blue pixel
+
+        # Modify the image with the inverted pixel values
+
+        img5.putpixel((i, j), (redPixel, greenPixel, bluePixel));
+
+# Display the negative image
+
+img5.show();
+
+# Convert RGB image into Black And White
+img6 = img6.convert('1')
+img6.show();
